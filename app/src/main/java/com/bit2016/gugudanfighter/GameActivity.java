1745edf7ef705d7f3f652da53939d0c31f7f1885
motherfather <mother@father>
2016-11-30 20:04:06 +0900

    주석 추가와 약간의 수정

--- app/src/main/java/com/bit2016/gugudanfighter/GameActivity.java
+++ app/src/main/java/com/bit2016/gugudanfighter/GameActivity.java
@@ -14,18 +14,18 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
-    private static final int LIMIT = 5;
-    private Timer timer = new Timer();
-    private TextView tvLastTime;
+    private static final int LIMIT = 5; // 제한 시간 설정
+    private Timer timer = new Timer(); // 타이머 선언
+    private TextView tvLastTime; // 타이머 출력 선언
 
     // 답
-    private static int LEFT_ANSWER;
-    private static int RIGHT_ANSWER;
+    private static int LEFT_ANSWER; // 답의 왼쪽 피연산자
+    private static int RIGHT_ANSWER; // 답의 오른쪽 피연산자
 
-    private static Set<Multiplication> set;
+    private static Set<Multiplication> set; // 중복값을 막기 위한 hashset 선언
 
-    private static int TOTAL;
-    private static int CORR;
+    private static int TOTAL; // 문제수
+    private static int CORR; // 정답수
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +34,10 @@ public class GameActivity extends AppCompatActivity {
 
         // 타이머 표시와 설정
         tvLastTime = (TextView)findViewById(R.id.textView_sec);
-        timer.schedule(new GameTimerTask(), 0, 1000);
+        timer.schedule(new GameTimerTask(), 0, 1000); // 0초부터 1초마다 시간 감소를 나타내기 위해서
 
         // 게임 시작
-        startGame();
+        startGame(); // 처음 문제와 답을 출력
     }
 
     // 뒤로가기 누를 때 타이머 끄고 액티비티 끄기
@@ -45,38 +45,47 @@ public class GameActivity extends AppCompatActivity {
     public void onBackPressed() {
         timer.cancel();
         // 매번 값을 따로 초기화 하지 않고 하는 법
-        TOTAL = 0;
+        TOTAL = 0; // 초기화를 안하면 뒤로가기해도 이전 문제 풀 때 있는 카운터가 넣어와서 초기화를 해줘야함
         CORR = 0;
         finish();
     }
 
     private void startGame() {
+        // 숫자 결정
         selectNumber();
+        // 문제 출력
         ((TextView)findViewById(R.id.textView_pre)).setText("" + LEFT_ANSWER);
         ((TextView)findViewById(R.id.textView_post)).setText("" + RIGHT_ANSWER);
-
+        // 정답수/문제수 출력
         ((TextView)findViewById(R.id.textView_num)).setText("" + CORR);
         ((TextView)findViewById(R.id.textView_num3)).setText("" + TOTAL);
 
+        // 버튼 배열화
         int[] button_id = {R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
-        int index = 0;
-        for (Multiplication mul : set) {
-            TextView textButton = ((TextView)findViewById(button_id[index]));
-            textButton.setText("" + (mul.left * mul.right));
+
+        int index = 0; // foreach문을 쓰게 되어서 button_id 배열에 필요한 i(인덱스)가 없어서 필요함...
+        for (Multiplication mul : set) { // foreach문
+            TextView textButton = (TextView)findViewById(button_id[index]); // R.id.button1 ~ button9 까지를 텍스트뷰로 만듬
+            textButton.setText("" + (mul.left * mul.right)); // 텍스트뷰로 만들어서 버튼에 텍스트를 입력하는 데 Multiplication mul에서 두 개의 피연산자를 가져옴
+            // 정답을 눌렀을 때...
             if ((textButton.getText()).equals(String.valueOf(LEFT_ANSWER * RIGHT_ANSWER))) {
                 textButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        // 정답카운트 증가, 문제카운트 증가, 새로운 문제 생성!!
                         CORR++;
                         TOTAL++;
                         startGame();
                     }
                 });
+            // 오답을 눌렀을 때...
             } else {
                 textButton.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        // 문제카운트 증가
                         TOTAL++;
+                        // 새로운 문제 생성!!
                         startGame();
                     }
                 });
@@ -102,10 +111,11 @@ public class GameActivity extends AppCompatActivity {
             set.add(new Multiplication(left, right));
         }
 
-        int indexRandom = randomize(0, 9); // hashset의 9개의 보기에서 문제 선택
+        int indexRandom = randomize(0, 9); // hashset의 9개의 보기에서 문제를 선택하기 위해서...
 
         int index = 0;
         for (Multiplication mul : set) {
+            // 문제 결정
             if (index == indexRandom) {
                 LEFT_ANSWER = mul.left;
                 RIGHT_ANSWER = mul.right;
@@ -132,9 +142,10 @@ public class GameActivity extends AppCompatActivity {
                     ", product=" + right*left +
                     '}';
         }
+
         // 결과값이 같을 때 중복이 되지 않도록 하기 위해서
         @Override
-        public boolean equals(Object o) {
+        public boolean equals(Object o) { // 4*6, 6*4, 8*3, 3*8과 같은 같은 곱한 값이 중복되는 것을 막기 위해서...
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
 
@@ -145,7 +156,7 @@ public class GameActivity extends AppCompatActivity {
         // 해시코드 생성
         @Override
         public int hashCode() {
-            return 31 * (left * right);
+            return 31 * left + right; // 3*9, 3*9 동일한 값이 여러번 들어오는 것을 막기 위해서...
         }
     }
 
@@ -160,13 +171,13 @@ public class GameActivity extends AppCompatActivity {
         @Override
         public void run() {
             seconds++;
-            if (seconds >= LIMIT) {
-                timer.cancel();
+            if (seconds >= LIMIT) { // 타이어가 끝나면...
+                timer.cancel(); // 타이머 종료
                 Intent intent = new Intent(GameActivity.this, ResultActivity.class);
-                intent.putExtra("total", TOTAL);
-                intent.putExtra("corr", CORR);
+                intent.putExtra("total", TOTAL); // 문제수와
+                intent.putExtra("corr", CORR); // 정답수를 ResultActivity로 넘기기 위해서 intent에 값을 담는 과정
                 startActivity(intent);
-                TOTAL = 0;
+                TOTAL = 0; // 다음 게임을 위한 초기화 과정
                 CORR = 0;
                 finish();
             }
